13ddb4154cdb4df09608d2bef9999195487f9b08
Swayamshree <gswayamshree2007@gmail.com>
2026-06-25 17:36:10 +0530

    feat: add application routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,14 @@
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Results from "./pages/Results";
 
 function App() {
-  return <Home />;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/results" element={<Results />} />
+    </Routes>
+  );
 }
 
 export default App;
